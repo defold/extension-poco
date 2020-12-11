07b68a7a1914ed9e9108f1a7da2633a87dd5f5d3
JCash <mwesterdahl76@gmail.com>
2020-12-11 15:47:52 +0100

    Updated level example script to handle the case when the targets are out of screen bounds

--- examples/example_level2.py
+++ examples/example_level2.py
@@ -22,12 +22,45 @@ poco = StdPoco(addr=(options.address, options.port), device=device, use_airtest_
 print("Loading level 2")
 poco("bn_level2/larrybutton").click()
 
+def is_outside(pos):
+    return pos[0] < 0.0 or pos[0] >= 1.0 or pos[1] < 0.0 or pos[1] > 1.0
+
+def project_to_border(pos):
+    # clip the line to the screen border
+    playerx, playery = 0.5, 0.5
+    diffx, diffy = pos[0] - playerx, pos[1] - playery
+
+    limitx, limity = 1.0, 1.0
+    if diffx < 0:
+        limitx = 0.0
+    if diffy < 0:
+        limity = 0.0
+
+    tx = (limitx - playerx) / diffx
+    ty = (limity - playery) / diffy
+    t = min(tx, ty)
+
+    return (playerx + t * diffx, playery + t * diffy)
+
 # In this test, we will go to each object in a certain sequence
-animal_names = ["/parrot1", "/parrot4", "/parrot", "/parrot2", "/parrot3"]
+animal_names = ["/parrot4", "/parrot1", "/parrot", "/parrot2", "/parrot3"]
 for animal in animal_names:
     print("Go to", animal)
     child = poco(animal).child('sprite')
-    print(animal, child.get_position(), child.get_size())
+
+    pos = child.get_position()
+
+    # If the position is outside of the screen, we approach the target by
+    # clicking on the line towards the goal until the target is on screen
+    while is_outside(pos):
+        clickx, clicky = project_to_border(pos)
+
+        print("click", clickx, clicky)
+        poco.agent.input.click(clickx, clicky)
+        time.sleep(0.6)
+
+        pos = child.get_position()
+
     child.click(sleep_interval=0.01)
     time.sleep(0.6)
 
